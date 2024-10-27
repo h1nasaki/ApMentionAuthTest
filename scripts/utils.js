78099160fa3e179c4e6da60b2c654f7ca44b9b52
h1nasaki <6a8372@gmail.com>
2024-10-27 16:20:32 +0900

    utils修正

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,11 +2,15 @@
  * Generate random 4-byte key
  */
 export function generateKey() {
+    console.group(`utils.js/generateKey`)
+    console.log(`called`)
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let result = '';
     for (let i = 0; i < 4; i++) {
         result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
+    console.log(`generated: ${result}`)
+    console.groupEnd()
     return result;
 }
 
@@ -14,16 +18,28 @@ export function generateKey() {
  * Save an object to local storage
  */
 export function saveLocalStorage(key, object) {
+    console.group(`utils.js/saveLocalStorage`)
+    console.log(`called for: ${key}`)
+    console.dir(object)
     const jsonString = JSON.stringify(object);
     localStorage.setItem(key, jsonString); // Replace 'myData' with your desired key name
+
+    console.groupEnd()
 }
 
 /**
  * Read an object from local storage
  */
 export function loadLocalStorage(key) {
-    const jsonString = localStorage.getItem(key);
+    console.group(`utils.js/loadLocalStorage`)
+    console.log(`called for: ${key}`)
 
+    const obj = localStorage.getItem(key);
+    console.dir(obj)
+
+    const jsonString = JSON.stringify(obj);
+
+    console.groupEnd()
     if (jsonString) {
         return JSON.parse(jsonString);
     } else {
