64d395c0b03e0cd690d2ff97ab3a2104d6d8dff8
h1nasaki <6a8372@gmail.com>
2024-10-27 16:13:52 +0900

    auth.js修正

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,7 +1,6 @@
 import { generateKey, saveLocalStorage, loadLocalStorage } from './utils.js'
 import { getMentions } from './mkApi.js'
 
-
 export function registRequest(username) {
 
     console.log(`registRequest Called: ${username}`)
@@ -21,31 +20,55 @@ export function registRequest(username) {
 }
 
 export async function checkAuthState(username) {
+    console.group(`auth.js/checkAuthState`)
+    console.log(`called for: ${username}`)
+
     const obj = loadLocalStorage(username)
+    // ローカルストレージに登録されていないユーザーの場合はこの時点で認証失敗
     if (obj === null) {
+        // ログ
+        console.warn('該当ユーザーはLocalStorageに登録されていません')
+        console.warn('認証失敗: 未登録')
+        console.groupEnd()
+
         return '認証失敗: 未登録'
     }
 
+    console.info(`Found: ${obj.username}/${obj.key}`)
+
+    // フラグ初期化
     let flg = false
 
+    // アカウントに到達したメンションを取得→合致するものがあるか確認
+    console.log('Fetching mentions...')
     await getMentions().then((notes) => {
         console.log(`Fetched ${notes.length} notes`)
-        console.log(`Finding: ${obj.key}`)
+        console.dir(notes)
+        console.group('checkMentions')
         for (const note of notes) {
+            console.group('checkNote')
             console.log(`username: @${note.user.username}@${note.user.host} / text: ${note.text}`)
-            if (`@${note.user.username}@${note.user.host}` === username) {
-                if (note.text.includes(obj.key)) {
+            if (`@${note.user.username}@${note.user.host}` === username) { // ターゲットユーザーが作成した投稿か?
+                console.log('ターゲットユーザーか? => Yes')
+                if (note.text.includes(obj.key)) { // メンションにキーが含まれているか?
+                    // どちらも合致した場合、認証成功
+                    // ログ
+                    console.log('キーが含まれているか? => Yes => 認証成功')
                     flg = true
-                }
-            }
+                } else console.log('キーが含まれているか? => No => Skip')
+            } else console.log('ターゲットユーザーか? => No => Skip')
+            console.groupEnd()
         }
+        console.groupEnd()
     })
 
+    console.log(`flg: ${flg}`)
+    console.groupEnd()
+
+    // 表示する文字列を返却
     if (flg) {
-        console.log('OK')
         return '認証成功: メンション検知'
     } else {
-        console.log('NG')
         return '認証失敗: メンション未達'
     }
 
